6493cf5c001983352bfc76395428a738410c0c5e
zbaishancha <zbaishancha@126.com>
2024-08-07 12:50:52 +0000

    support loss weights

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -23,10 +23,17 @@ from transformers import AutoConfig, AutoModelForCausalLM, \
 
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.generation.utils import GenerateOutput
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from transformers.models.llama.modeling_llama import LLAMA_INPUTS_DOCSTRING
+from transformers.utils import (add_start_docstrings_to_model_forward,
+                                replace_return_docstrings)
+
+from llava.constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX
 
 from ..llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
 
-
+_CONFIG_FOR_DOC = "LlamaConfig"
 class LlavaConfig(LlamaConfig):
     model_type = "llava_llama"
 
@@ -47,12 +54,162 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         self.pretraining_tp = config.pretraining_tp
         self.vocab_size = config.vocab_size
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
-
+        self.loss_weights = False
         # Initialize weights and apply final processing
         self.post_init()
 
     def get_model(self):
         return self.model
+
+    def js_divergence(self, p, q):
+        m = 0.5 * (p + q)
+        return 0.5 * (F.kl_div(m.log(), p, reduction='batchmean') + F.kl_div(m.log(), q, reduction='batchmean'))
+
+    def calculate_weights(self, shift_logits_no_img, shift_logits, labels):
+        b, l, c = shift_logits.shape
+
+        fake_data = torch.randn(b, l-shift_logits_no_img.size(1), c, device=labels.device)
+        shift_logits_no_img = torch.cat([fake_data, shift_logits_no_img], dim=1)
+        shift_logits_no_img_probs = F.softmax(shift_logits_no_img, dim=-1)
+        shift_logits_probs = F.softmax(shift_logits, dim=-1)
+
+        weights = torch.zeros(b, l, device=labels.device)
+        for i in range(b):
+            for j in range(l):
+                if int(labels[i, j]) != -100:
+                    weights[i, j] = self.js_divergence(shift_logits_no_img_probs[i, j], 
+                                                    shift_logits_probs[i, j])
+                else:
+                    weights[i, j] = float('-inf')
+        
+        weights = F.softmax(weights, dim=-1)
+        return weights
+
+    @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+    @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
+    def forward_with_weights(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        shift_logits_no_img: Optional[torch.Tensor] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        r"""
+        Args:
+            labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        Returns:
+
+        Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, LlamaForCausalLM
+
+        >>> model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+        >>> tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+        >>> prompt = "Hey, are you conscious? Can you talk to me?"
+        >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+        >>> # Generate
+        >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+        >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+        ```"""
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            
+            # loss weights if true
+            weights = None
+            if shift_logits_no_img is not None:
+                weights = self.calculate_weights(shift_logits_no_img.clone(), shift_logits.clone(), shift_labels.clone())
+                weights = weights.view(-1)
+            
+            # Flatten the tokens
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+
+            if weights is not None:
+                loss_fct = CrossEntropyLoss(reduction='none')
+                loss = loss_fct(shift_logits, shift_labels)
+                loss = loss * weights
+                loss = loss.sum()
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(shift_logits, shift_labels)
+        
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+    def remove_values(self, input_ids, labels, attention_mask, value=IMAGE_TOKEN_INDEX):
+        b, l = input_ids.shape
+        new_tensor1 = []
+        new_tensor2 = []
+        new_tensor3 = []
+
+        for i in range(b):
+            index = (input_ids[i] == value).nonzero(as_tuple=True)[0].item()
+            new_tensor1.append(torch.cat((input_ids[i, :index], input_ids[i, index+1:])))
+            new_tensor2.append(torch.cat((labels[i, :index], labels[i, index+1:])))
+            new_tensor3.append(torch.cat((attention_mask[i, :index], attention_mask[i, index+1:])))
+
+        input_ids = torch.stack(new_tensor1)
+        labels = torch.stack(new_tensor2)
+        attention_mask = torch.stack(new_tensor3)
+        return input_ids, labels, attention_mask
 
     def forward(
         self,
@@ -70,6 +227,43 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
+        shift_logits_no_img = None
+        if self.loss_weights:
+            self.eval()
+            with torch.no_grad():
+                (
+                    input_ids_no_img,
+                    position_ids_no_img,
+                    attention_mask_no_img,
+                    past_key_values_no_img,
+                    inputs_embeds_no_img,
+                    labels_no_img,
+                ) = self.prepare_inputs_labels_for_multimodal(
+                    input_ids,
+                    position_ids,
+                    attention_mask,
+                    past_key_values,
+                    labels,
+                    images=None
+                )
+                input_ids_no_img, labels_no_img, attention_mask_no_img  = \
+                            self.remove_values(input_ids_no_img, labels_no_img, attention_mask_no_img)
+
+                output = self.forward_with_weights(
+                        input_ids=input_ids_no_img,
+                        attention_mask=attention_mask_no_img,
+                        position_ids=position_ids_no_img,
+                        past_key_values=past_key_values_no_img,
+                        inputs_embeds=inputs_embeds_no_img,
+                        labels=None,
+                        use_cache=use_cache,
+                        output_attentions=output_attentions,
+                        output_hidden_states=output_hidden_states,
+                        return_dict=return_dict)
+                
+                shift_logits_no_img = output.logits[..., :-1, :].contiguous()
+            self.train()
+        
         if inputs_embeds is None:
             (
                 input_ids,
@@ -88,7 +282,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 image_sizes
             )
 
-        return super().forward(
+        return self.forward_with_weights(
             input_ids=input_ids,
             attention_mask=attention_mask,
             position_ids=position_ids,
@@ -98,7 +292,8 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             use_cache=use_cache,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
-            return_dict=return_dict
+            return_dict=return_dict,
+            shift_logits_no_img=shift_logits_no_img,
         )
 
     @torch.no_grad()
