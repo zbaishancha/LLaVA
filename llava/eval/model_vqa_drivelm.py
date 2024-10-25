aa78c149b6238bd1229a236532b82453b571eda2
Camellia-hz <z_baishancha@126.com>
2024-10-25 13:39:28 +0000

    suooprt drivelm

---
+++ llava/eval/model_vqa_drivelm.py
@@ -0,0 +1,135 @@
+import argparse
+import torch
+import os
+import json
+from tqdm import tqdm
+import shortuuid
+import transformers
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.train.train import CLIP_PATH
+from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
+
+from PIL import Image
+import math
+
+TEXT = "All objects."
+
+def split_list(lst, n):
+    """Split a list into n (roughly) equal-sized chunks"""
+    chunk_size = math.ceil(len(lst) / n)  # integer division
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
+
+
+def get_chunk(lst, n, k):
+    chunks = split_list(lst, n)
+    return chunks[k]
+
+
+def eval_model(args):
+    # Model
+    disable_torch_init()
+    model_path = os.path.expanduser(args.model_path)
+    model_name = get_model_name_from_path(model_path)
+    tokenizer, model, image_processor, context_len, prompt_image_processor, object_processor = load_pretrained_model(model_path, args.model_base, model_name)
+    clip_tokenizer = transformers.AutoTokenizer.from_pretrained(CLIP_PATH)
+    if "lingoqa" in args.question_file.lower() \
+        or "drivelm" in args.question_file.lower():
+        with open(args.question_file, 'r', encoding='utf-8') as file:  
+            data = json.load(file)
+        questions = []
+        for example in data:
+            question_id = example['id']
+            image_path_list = example['image']
+            text = example['conversations'][0]['value'].replace("<image>\n", "")
+            questions.append(dict(question_id=question_id, image_path_list=image_path_list, text=text))
+    else:
+        questions = [json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")]
+    questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
+    answers_file = os.path.expanduser(args.answers_file)
+    os.makedirs(os.path.dirname(answers_file), exist_ok=True)
+    ans_file = open(answers_file, "w")
+    if "lingoqa" in args.question_file.lower():
+        questions = questions[::2]    
+    for line in tqdm(questions):
+        idx = line["question_id"]
+        image_path_list = line["image_path_list"]
+        qs = line["text"]
+        cur_prompt = qs
+        
+        question_ids = tokenizer(qs,
+                return_tensors="pt",
+                padding='max_length',
+                max_length=20,
+                truncation=True).input_ids
+        
+        if model.config.mm_use_im_start_end:
+            qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+
+        conv = conv_templates[args.conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+        
+        images = []
+        for img_path in image_path_list:
+            if "nuscenes" in img_path:
+                img = Image.open(os.path.join(args.image_folder, img_path).replace('data/nuscenes/samples/', "")).convert('RGB')
+            else:
+                img = Image.open(os.path.join(args.image_folder, img_path)).convert('RGB')
+            images.append(img)
+        
+        image_tensor = process_images(images, image_processor, model.config)
+        prompt_images = process_images(images, prompt_image_processor, model.config)
+        object_images = process_images(images, object_processor, model.config)
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=image_tensor.unsqueeze(0).half().cuda(),
+                image_sizes=[images[0].size],
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                num_beams=args.num_beams,
+                # no_repeat_ngram_size=3,
+                max_new_tokens=1024,
+                use_cache=True,
+                question_ids=question_ids.cuda(),
+                prompt_images=prompt_images.unsqueeze(0).half().cuda(),
+                object_images=object_images.unsqueeze(0).half().cuda())
+
+        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+
+        ans_id = shortuuid.uuid()
+        ans_file.write(json.dumps({"question_id": idx,
+                                   "prompt": cur_prompt,
+                                   "text": outputs,
+                                   "answer_id": ans_id,
+                                   "model_id": model_name,
+                                   "metadata": {}}) + "\n")
+        ans_file.flush()
+    ans_file.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
+    parser.add_argument("--model-base", type=str, default=None)
+    parser.add_argument("--image-folder", type=str, default="")
+    parser.add_argument("--question-file", type=str, default="tables/question.jsonl")
+    parser.add_argument("--answers-file", type=str, default="answer.jsonl")
+    parser.add_argument("--conv-mode", type=str, default="v1")
+    parser.add_argument("--num-chunks", type=int, default=1)
+    parser.add_argument("--chunk-idx", type=int, default=0)
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--top_p", type=float, default=None)
+    parser.add_argument("--num_beams", type=int, default=1)
+    args = parser.parse_args()
+
+    eval_model(args)
